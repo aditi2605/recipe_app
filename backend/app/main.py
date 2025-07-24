e45190ee55689306f68cc5a0ecd2b54b2014b59f
Aditi Parikh <aditi.parikh0526@gmail.com>
2025-07-24 10:13:34 +0100

    added vercel connection url to connect front to back

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,10 +10,16 @@ Base.metadata.create_all(bind=engine)
 
 app  = FastAPI()
 
+origins = [
+    "https://bitecult.vercel.app",
+    "http://localhost:3000",
+
+]
+
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"], 
+    allow_origins=origins, 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
